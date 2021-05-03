c9b640208f47355beca5150d80c3961ad8ce5d29
DumaHycc <nuss_dmitriy@mail.ru>
2021-05-03 17:46:51 +0400

    is_hex_number to hex_to_dec

--- NULLTerminatedLinesHW/main.cpp
+++ NULLTerminatedLinesHW/main.cpp
@@ -11,18 +11,23 @@ void to_upper(char str[]);         //Переводит строку в верх
 void to_lower(char str[]);         //Переводит строку в нижний регистр
 void capitalize(char str[]);       //Первую букву каждого слова в предложении делает заглавной
 void shrink(char str[]);           //Удаляет из строки лишние пробелы, например:
-								   //Хорошо      живет   на  свете   Винни   Пух
+void shrink_two(char str[]);	   //Хорошо      живет   на  свете   Винни   Пух
+
+void remove_symbol(char str[], char symbol);
 bool is_palindrome(char str[]);    //Определяет, является ли строка палиндромом
+bool is_palindrome_two(char str[]);
+
 bool is_int_number(char str[]);    //Определяет, является ли строка целым числом
 								   //Строка является целым числом, когда она состоит только из цифр.
 int to_int_number(char str[]);     //Если строка - целое число, функция вернет его числовое значение.
 
 bool is_bin_number(char str[]);    //Проверяет, является ли строка двоичным числом
-//int bin_to_dec(char str[]);      //Если строка - двоичное число, функция вернет его десятичное значение.
+int bin_to_dec(char str[]);        //Если строка - двоичное число, функция вернет его десятичное значение.
+int bin_to_dec_two(char str[]);
 //??? dec_to_bin(int decimal);     //Функция принимает десятичное число, и возвращает его двоичное значение.
 
-//bool is_hex_number(char str[]);  //Проверяет, является ли функция шестнадцатеричным числом
-//int hex_to_dec(char str[]);      //Если строка - Шестнадцатеричное число, функция вернет его десятичное значение.
+bool is_hex_number(char str[]);    //Проверяет, является ли функция шестнадцатеричным числом
+int hex_to_dec(char str[]);        //Если строка - шестнадцатеричное число, функция вернет его десятичное значение.
 //??? dec_to_hex(int decimal);     //Функция принимает десятичное число, и возвращает его Шестнадцатеричное значение.
 
 //bool is_mac_address(char str[]); //Проверяет, является ли строка MAC-адресом
@@ -52,22 +57,38 @@ void main()
 	to_upper(str);
 	cout << ".........to_upper: " << str << endl;
 
+	to_lower(str);
+	cout << ".........to_lower: " << str << endl;
+
+	shrink(str);
 	cout << "...........shrink: " << str << endl;
+	cout << ".......shrink_two: " << str << endl;
 
 	capitalize(str);
 	cout << ".......capitalize: " << str << endl;
 
 	cout << ".......palindrome: ";
-	cout << (is_palindrome(str) == 0 ? "Строка не является палиндромом" : "Строка является палиндромом") << endl;
-	//cout << "Строка " << (is_palindrome(str) ? "" : "не ") << "является палиндромом" << endl;
+	cout << "Строка " << (is_palindrome(str) ? "" : "не ") << "является палиндромом" << endl;
+
+	cout << "...palindrome_two: ";
+	cout << "Строка " << (is_palindrome_two(str) ? "" : "не ") << "является палиндромом" << endl;
 
 	cout << "....is_int_number: ";
-	cout << (is_int_number(str) == 0 ? "Строка не является числом" : "Строка является числом") << endl;
+	cout << "Строка " << (is_int_number(str) ? "" : "не ") << "является числом" << endl;
 
 	cout << "....to_int_number: " << to_int_number(str) << endl;
 
 	cout << "....is_bin_number: ";
-	cout << (is_bin_number(str) == 0 ? "Строка не является двоичным числом" : "Строка является двоичным числом") << endl;
+	cout << "Строка " << (is_bin_number(str) ? "" : "не ") << "является двоичным числом" << endl;
+
+	cout << ".......bin_to_dec: ";
+	cout << bin_to_dec(str) << endl;
+
+	cout << "...bin_to_dec_two: ";
+	cout << bin_to_dec_two(str) << endl;
+
+	cout << "....is_hex_number: ";
+	cout << "Строка " << (is_hex_number(str) ? "" : "не ") << "является шестнадцатеричным числом" << endl;
 }
 
 void Input(char str[], const int n)
@@ -120,7 +141,7 @@ void capitalize(char str[])
 		str[0] -= 32;
 	for (int i = 1; str[i]; i++) //Поскольку начальный символ уже проверили, то можно начинать с единицы int i = 1.
 	{
-		if (str[i - 1] == ' ') //Если предыдущий смвол пробел.
+		if (str[i - 1] == ' ') //Если предыдущий символ пробел.
 		{
 			if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я')
 				str[i] -= 32; //Тогда отнимаем 32.
@@ -144,6 +165,57 @@ void shrink(char str[])
 	}
 }
 
+void shrink_two(char str[])
+{
+	//Частный случай: убирает в начале пробелы
+	//for (int j = 0; str[j]; j++)
+	//Общий случай:
+	for (int i = 0; str[i]; i++)
+	{
+		//Пока элемент стоки равен пробелу и следующий элемент равен пробелу или 0
+		if (
+			str[i] == ' ' &&
+			(
+				str[i + 1] == ' ' ||
+				str[i + 1] == '.' ||
+				str[i + 1] == ',' ||
+				str[i + 1] == '?' ||
+				str[i + 1] == '!' ||
+				str[i + 1] == ':' ||
+				str[i + 1] == ';' ||
+				str[i + 1] == '-' ||
+				str[i + 1] == ')'
+				)
+			|| str[0] == ' '
+			|| str[i - 1] == '(' && str[i] == ' '
+			|| str[i - 1] == '-' && str[i] == ' '
+			)
+		{
+			//То сдвигаем в лево строку
+			for (int j = i; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+			i--;
+		}
+	}
+}
+
+void remove_symbol(char str[], char symbol)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] == symbol)
+		{
+			for (int j = i; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+			i--;
+		}
+	}
+}
+
 bool is_palindrome(char str[])
 {
 	//Узнаем размер строки
@@ -160,26 +232,61 @@ bool is_palindrome(char str[])
 	return true;
 }
 
+bool is_palindrome_two(char str[])
+{
+	//Узнаем размер строки
+	int size = StringLength(str);
+	char* buffer = new char[size + 1]{};
+	strcpy_s(buffer, size + 1, str); //Копирует строку str в сторку buffer
+	to_lower(buffer);
+	remove_symbol(buffer, ' ');
+	size = StringLength(buffer);
+	for (int i = 0; i < size / 2; i++)
+	{
+		//Если первый символ не равен последнему или второй не равен предпоследнему и т.д.
+		if (buffer[i] != buffer[size - 1 - i])
+		{
+			delete[] buffer;
+			return false;
+		}
+	}
+	delete[] buffer;
+	return true;
+}
+
 bool is_int_number(char str[])
 {
+	remove_symbol(str, ' ');
 	for (int i = 0; str[i]; i++)
 	{
-		if (!(str[i] >= '0' && str[i] <= '9'))
+		//Если элемент строки не цифра
+		//Если элемент строки строго больше или равно 0 и элемент строки меньше или равно 9
+		//и элемент строки не является пробелом
+		if (!(str[i] >= '0' && str[i] <= '9') && str[i] != ' ')
+			return false;
+		//Если текущий элемент - пробел и следующий пробел
+		if (str[i] == ' ' && str[i + 1] == ' ')
 			return false;
 	}
+	//Если же отрицание не выполнилось, то все элементы строки являются цифрами
 	return true;
 }
 
 int to_int_number(char str[])
 {
-	if (!is_int_number(str)) return 0;
-	int num = 0;
+	remove_symbol(str, ' ');
+	if (!is_int_number(str))
+		return 0;
+	int num = 0; //Значение числа
 	for (int i = 0; str[i]; i++)
 	{
+		//Если элемент строки не пробел,
 		if (str[i] != ' ')
 		{
-			num *= 10;
-			num += str[i] - 48;
+			//тогда значение числа умножаем на 10.
+			num *= 10; //Сдвигаем число на один разряд влево, чтобы освободить младший разряд для следующей цифры
+			//После того как сдвинули число на целый разряд, к num прибавляем значение строки 
+			num += str[i] - 48; //48 - ASCII-код символа '0'
 		}
 	}
 	return num;
@@ -191,6 +298,85 @@ bool is_bin_number(char str[])
 	{
 		if (str[i] != '0' && str[i] != '1' && str[i] != ' ')
 			return false;
+		//Предыдущий элемент строки - пробел и текущий элемент строки - пробел и следующий элемент строки пробел
+		if (str[i - 1] == ' ' && str[i] == ' ' && str[i + 1] == ' ')
+			return false;
 	}
 	return true;
+}
+
+int bin_to_dec(char str[])      //Перевод двоичного числа в десятичную систему счисления
+{
+	if (!is_bin_number(str))
+		return 0;
+	int decimal = 0;            //Конечное десятичное число 
+	int weight = 1;             //Весовой коэффициент у младшего разряда - единица.
+	int n = StringLength(str);  //Разрядность числа
+	for (int i = n - 1; i >= 0; i--)
+	{
+		//Если элемент строки не пробел,
+		if (str[i] != ' ')
+		{
+			//тогда к десятичному значению числа прибавляем значение разряда (-48) * на весовой коэффициент weight
+			decimal += (str[i] - 48) * weight;
+			//После чего переходим к следующему весовому коэффициенту
+			weight *= 2;
+		}
+	}
+	return decimal;
+}
+
+int bin_to_dec_two(char str[])
+{
+	if (!is_bin_number(str))
+		return 0;
+	int decimal = 0;
+	remove_symbol(str, ' ');
+	int n = StringLength(str);
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			decimal += (str[i] - 48) * pow(2, --n);
+		}
+	}
+	return decimal;
+}
+
+bool is_hex_number(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (
+			!(str[i] >= '0' && str[i] <= '9') &&
+			!(str[i] >= 'a' && str[i] <= 'f') &&
+			!(str[i] >= 'A' && str[i] <= 'F') &&
+			str[i] != ' '
+			)
+		{
+			return false;
+		}
+		if (str[i] == ' ' && str[i + 1] == ' ')return false;
+	}
+	return true;
+}
+
+int hex_to_dec(char str[])
+{
+	if (!is_hex_number(str))return 0;
+	int n = StringLength(str);
+	char* buffer = new char[n + 1]{};
+	strcpy(buffer, str);
+	int decimal = 0;
+	int weight = 1;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (str[i] != ' ')
+		{
+			decimal += (str[i] - 48) * weight;
+			weight *= 16;
+		}
+		if (str[i] == 'x' || str[i] == 'X')break;
+	}
+	return decimal;
 }
